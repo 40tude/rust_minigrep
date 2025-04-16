c07ac6e9249a5220149a1c455e2ef182c4515613
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-04-16 11:39:43 +0200

    p256

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,20 @@ struct Config {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let (query, file_path) = parse_config(&args);
+    // if Result is an OK value, .unwrap_or_else() returns the inner value wrapped by Ok
+    // On Err value, the closure gets called
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments : {err}");
+        println!("Problem parsing arguments : {err}"); // see the content of Err in Config::build()
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text :\n{contents}");
