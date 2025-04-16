73c3fd209edc512689b07fedfd80a6e169c00aa4
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-04-16 11:21:12 +0200

    p255

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 struct Config {
     query: String,
     file_path: String,
@@ -9,7 +10,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // let (query, file_path) = parse_config(&args);
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments : {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,13 +24,13 @@ fn main() {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments")
+            return Err("Not enough arguments");
         }
         let query = args[1].clone(); // at this stage it is OK to .clone()
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
