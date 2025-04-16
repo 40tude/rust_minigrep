a85996d0030a7665ee32386d319fbabdfa2b54cb
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-04-16 12:00:54 +0200

    p259

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs;
+
+// pub is everywhere...
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone(); // at this stage it is OK to .clone()
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+// In case of Ok case, the function still return unit type ()
+// For error type, use the trait object Box<dyn Error> (see chap 17)
+// run will return a type that implement the Error trait but we don't specify the particular type
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text :\n{contents}");
+    Ok(()) // wrap the unit type () in the OK()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
-struct Config {
-    query: String,
-    file_path: String,
-}
+
+use minigrep::Config; // bring the Config type from the lib crate into the binary scope
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,29 +16,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    // prefix the run with lib crate name
+    if let Err(e) = minigrep::run(config) {
         println!("Application error : {e}");
         process::exit(1);
-    }
-}
-
-// In case of Ok case, the function still return unit type ()
-// For error type, use the trait object Box<dyn Error> (see chap 17)
-// run will return a type that implement the Error trait but we don't specify the particular type
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text :\n{contents}");
-    Ok(()) // wrap the unit type () in the OK()
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone(); // at this stage it is OK to .clone()
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
